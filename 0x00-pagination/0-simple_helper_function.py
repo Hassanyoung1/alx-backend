9706686a8b182a849e285cfaeda0fe7e4aff2dfb
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-04-01 13:12:35 +0100

    Simple

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+a function named index_range that takes two integer
+arguments page and page_size and returns a tuple containing
+"""
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
---
+++ 0x00-pagination/README.md
@@ -0,0 +1,2 @@
+# This is a project on pagination using Python and Flask
+# The project is a simple implementation of pagination using Python and Flask
