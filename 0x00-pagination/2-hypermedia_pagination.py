72737a8767017ce6f1b918f10b36e47fd33a1051
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-04-01 17:11:50 +0100

    Hypermedia

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+"""
+This module contains a single function index_range thati
+takes two integers
+"""
+
+from typing import List, Tuple
+import csv
+import math
+
+
+def index_range(page: int, page_size: int) -> Tuple:
+    """
+    a function named index_range that takes two integer
+    """
+    return (page - 1) * page_size,  (((page - 1) * page_size) + page_size)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        ""
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start > len(data):
+            return []
+        return data[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
+        data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        next_page = page + 1 if page + 1 < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+        return {
+            'page_size': len(data),
+            'page': page,
+            'data': data,
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_pages': total_pages
+        }
