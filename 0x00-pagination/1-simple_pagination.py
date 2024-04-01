eae2710d8432c17a98d630a6b2c13f2909028cdb
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-04-01 22:36:35 +0100

    A

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,29 +1,20 @@
 #!/usr/bin/env python3
+
 """
-Defines class Server that paginates a database of popular baby names
+This module contains a single function index_range thati
+takes two integers
 """
+
+from typing import List, Tuple
 import csv
 import math
-from typing import List, Tuple
 
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple:
     """
-    Takes 2 integer arguments and returns a tuple of size two
-    containing the start and end index corresponding to the range of
-    indexes to return in a list for those pagination parameters
-    Args:
-        page (int): page number to return (pages are 1-indexed)
-        page_size (int): number of items per page
-    Return:
-        tuple(start_index, end_index)
+    a function named index_range that takes two integer
     """
-    start, end = 0, 0
-    for i in range(page):
-        start = end
-        end += page_size
-
-    return (start, end)
+    return (page - 1) * page_size,  (((page - 1) * page_size) + page_size)
 
 
 class Server:
