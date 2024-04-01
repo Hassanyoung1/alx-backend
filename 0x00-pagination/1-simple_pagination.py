5061b3829520c7ab738dde581a66f31702af7372
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-04-01 15:37:24 +0100

    A

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -41,7 +41,8 @@ class Server:
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         pass
 
-        assert isinstance(page, int) and isinstance(page_size, int) > 0
+        assert type(page) is int and type(page_size) is int
+        assert page > 0 and page_size > 0
         start_index, end_index = index_range(page, page_size)
         data = self.dataset()
         if start_index > len(data):
