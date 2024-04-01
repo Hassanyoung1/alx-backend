cfc834635d3cf540bb82a3afe548d0539a96ef3c
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-04-01 13:52:33 +0100

    Simple

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,9 +4,12 @@
 a function named index_range that takes two integer
 arguments page and page_size and returns a tuple containing
 """
+from typing import Tuple
 
-
-def index_range(page: int, page_size: int) -> tuple:
+def index_range(page: int, page_size: int) -> tuple[int, int]:
+    """
+    a function named index_range that takes two integer
+    """
     start_index = (page - 1) * page_size
-    end_index = page * page_size
-    return (start_index, end_index)
+    end_index = start_index + page_size
+    return start_index, end_index
