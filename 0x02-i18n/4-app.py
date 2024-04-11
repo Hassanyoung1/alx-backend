3409c9fa3ea8f3db78be3725f7592a0cde296bfb
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-04-11 23:47:03 +0100

    Force

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+force a particular locale with URL parameter
+"""
 
 from flask import Flask, render_template, request
 from flask_babel import Babel
@@ -7,15 +10,24 @@ from jinja2.ext import Extension
 app = Flask(__name__)
 babel = Babel(app)
 
+
 class Config(object):
+    """
+    Config class
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app.config.from_object(Config)
+
 
 @babel.localeselector
 def get_locale():
+    """
+    get locale
+    """
     get_locale = request.args.get('locale')
     if get_locale in app.config['LANGUAGES']:
         return get_locale
@@ -24,7 +36,11 @@ def get_locale():
 
 @app.route('/', strict_slashes=False)
 def index():
+    """
+    index function
+    """
     return render_template('4-index.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5004, debug=True)
