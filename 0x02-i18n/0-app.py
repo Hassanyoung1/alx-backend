c340690fdde3f054a157131bec84e8e9a610744f
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-04-11 22:42:57 +0100

    basic

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,10 +8,12 @@ import flask_babel import Babel
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     """ Returns the index.html """
     return render_template('/0-index.html')
 
+
 if __name__ == '__main__':
-     app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
