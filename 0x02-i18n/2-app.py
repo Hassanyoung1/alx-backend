265335e8ec16357ebcbd6a373d542abfa039a0cc
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-04-11 23:39:02 +0100

    basic

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -8,6 +8,9 @@ from flask_babel import Babel
 
 
 class Config(object):
+    """
+    Configuration Class for Babel
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
