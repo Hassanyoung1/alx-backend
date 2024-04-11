b1b4841a6027a90d8745897fbd59a702ed49e952
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-04-11 23:28:17 +0100

    basic

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
+"""
+Module for trying out Babel i18n
+"""
 
 from flask import Flask, render_template
 from flask_babel import Babel
 
 
-
 app = Flask(__name__)
 babel = Babel(app)
+
 
 class Config(object):
     LANGUAGES = ["en", "fr"]
@@ -14,16 +17,27 @@ class Config(object):
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+app.config.from_object(Config)
+
+
 @babel.localeselector
 def get_locale():
+    """
+    Determine the best match for the supported languages
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
 
 
 @app.route('/', strict_slashes=False)
 def index():
-    return render_template('/2-index.html')
+    """
+    Render the index template
+    """
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
-     app.run(host='0.0.0.0', port=5000, debug=True)
+    """
+    Entry point
+    """
+    app.run(host='0.0.0.0', port=5000, debug=True)
