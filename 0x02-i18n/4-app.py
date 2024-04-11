3524a5aeb7685ee0832902bab1d83c99f82c35da
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-04-11 22:39:49 +0100

    i18n

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+from jinja2.ext import Extension
+
+app = Flask(__name__)
+babel = Babel(app)
+
+class Config(object):
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app.config.from_object(Config)
+
+@babel.localeselector
+def get_locale():
+    get_locale = request.args.get('locale')
+    if get_locale in app.config['LANGUAGES']:
+        return get_locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    return render_template('4-index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5004, debug=True)
