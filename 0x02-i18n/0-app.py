3e8324908b9744820c76df11ddb794af1c869150
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-04-11 22:56:19 +0100

    basic

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 """
-Flask app
+Basic flask setup
 """
-from flask import Flask, render_template
 
+from flask import Flask, render_template, request
+import flask_babel import Babel
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index() -> str:
-    """
-    Handles / route
-    """
+def index():
+    """ Returns the index.html """
     return render_template('0-index.html')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(port="5000", host="0.0.0.0", debug=True)
