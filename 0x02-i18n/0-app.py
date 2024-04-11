195673c0e4ea8e3550a7283caf5e1b4f0dad7d50
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-04-11 22:57:10 +0100

    basic

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
+def index() -> str:
     """ Returns the index.html """
     return render_template('0-index.html')
 
