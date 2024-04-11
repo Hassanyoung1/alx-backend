4e6a0cc17e74d093a4ded7074ae0a49bbc54bd2d
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-04-11 22:41:16 +0100

    asic

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+Basic Babel setup
+"""
 
 from flask import Flask, render_template, request
 import flask_babel import Babel
@@ -7,6 +10,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """ Returns the index.html """
     return render_template('/0-index.html')
 
 if __name__ == '__main__':
